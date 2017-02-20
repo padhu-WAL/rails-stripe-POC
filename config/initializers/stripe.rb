4ad57d33170390db1beef99c06c955a9685cb645
Padhu WAL <padhuwal@Padhus-MacBook-Pro.local>
2017-02-20 15:25:40 +0530

    remove credentials@

--- config/initializers/stripe.rb
+++
@@ -1,6 +0,0 @@
-Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_qyiJIlc2jm7QuNumP3BWqzoE',
-  :secret_key      => 'sk_test_RYwXnaKvXMC0NEmMMm3SUCAb'
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
